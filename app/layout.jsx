35e2920f46c132f941ffeb81dc65f0da7c70b11a
Ryan <ryansheikh003@gmail.com>
2024-02-09 00:52:25 -0700

    Completed homepage and navbar component

---
+++ app/layout.jsx
@@ -0,0 +1,24 @@
+import '@styles/globals.css';
+import Nav from '@components/Nav'
+export const metadata = {
+    title: "PromptVerse",
+    description: "Discover & Share AI prompts"
+}
+const RootLayout = ({children}) => {
+  return (
+    <html lang="en">
+        <body>
+            <div className="main">
+                <div className='gradient'></div>
+            </div>
+
+            <main className="app">
+                <Nav />
+                {children}
+            </main>
+        </body>
+    </html>
+  )
+}
+
+export default RootLayout;
---
+++ app/page.jsx
@@ -0,0 +1,19 @@
+import feed from '@components/Feed'
+
+
+const page = () =>{
+    return (
+        <section className="w-full flex-center flex-col">
+            <h1 className="head_text text-center">
+                Discover & Share
+                <br className="max-md:hidden"/>
+                <span className="orange_gradient"> AI-Powered Prompts</span>
+            </h1>
+            <p className="desc text-center">
+                PromptVerse is an open-source AI prompting tool for modern world to discover, create and share creative prompts
+            </p>
+        </section>
+    )
+}
+
+export default page;
